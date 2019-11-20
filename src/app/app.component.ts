2a3ac6ee668bf554a53d9b6bcbe4c5c2d1e0ef47
retevil <adolfo.sobrino@gmail.com>
2019-11-20 09:09:42 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular'; //angular taka
+  name = 'Angular'; //angular taka 2
 }
